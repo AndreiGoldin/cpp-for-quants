b44e51e3499a8001b4bddeb36b29ddae497fda2e
AndreiGoldin <andrei.goldin@skoltech.ru>
2022-06-17 17:19:07 +0300

    Add payoff hierarchy (Ch. 4)

---
+++ payoff.cpp
@@ -0,0 +1,40 @@
+#ifndef PAYOFF_CPP 
+#define PAYOFF_CPP
+
+#include "payoff.h"
+// #include <cmath>
+#include <iostream>
+
+PayOffCall::PayOffCall(const double _K) { K=_K; }
+PayOffCall::~PayOffCall() {}
+
+// Over−ridden operator () method , which turns PayOffCall object into a function
+double PayOffCall::operator() (const double S) const {
+    return std::max(S-K, 0.0);
+}
+
+PayOffDoubleDigital::PayOffDoubleDigital(const double _U, const double _D){
+    U = _U; D = _D;
+}
+PayOffDoubleDigital::~PayOffDoubleDigital() {};
+
+double PayOffDoubleDigital::operator() (const double S) const {
+    if (S>=D && S<=U){
+        return 1.0;
+    } else {
+        return 0.0;
+    }
+}
+
+#endif
+
+int main(){
+    PayOffCall default_call(100.0);
+    double default_price;
+    PayOffDoubleDigital another_call(110.0, 100.0);
+    double another_price;
+    default_price = default_call(120.0);
+    another_price = another_call(105.0);
+    std::cout << "Default call price: " << default_price << std::endl;
+    std::cout << "Non-default call price: " << another_price << std::endl;
+}
---
+++ payoff.h
@@ -0,0 +1,46 @@
+#ifndef PAYOFF_H
+#define PAYOFF_H
+
+#include <algorithm>
+
+class PayOff {
+    public:
+        PayOff() {}; // Default constructor
+        virtual ~PayOff() {}; // Virtual destructor
+
+        // Overloading () turns the PayOff into 
+        // an abstract function object
+        virtual double operator() (const double S) const = 0; // Pure virtual method
+};
+
+class PayOffCall : public PayOff {
+    private:
+        double K;
+    public:
+        PayOffCall(const double _K); // No default constructor
+        virtual ~PayOffCall(); // Virtual destructor
+
+        virtual double operator() (const double S) const;
+};
+
+class PayOffPut : public PayOff {
+    private:
+        double K;
+    public:
+        PayOffPut(const double K_); // No default constructor
+        virtual ~PayOffPut(); // Virtual destructor
+
+        virtual double operator() (const double S) const;
+};
+
+class PayOffDoubleDigital : public PayOff {
+    private:
+        double U; //Upper strike price
+        double D; //Lower strike price
+    public:
+        PayOffDoubleDigital(const double U_, const double D_);
+        virtual ~PayOffDoubleDigital();
+        virtual double operator() (const double S) const;
+};
+
+#endif
